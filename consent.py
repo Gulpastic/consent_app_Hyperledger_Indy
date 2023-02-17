a8400a0a8a50334ba0784ee4d3aa1fb216b2f284
root <root@Laptop.localdomain>
2023-02-17 18:45:02 +0530

    first commit

---
+++ consent.py
@@ -0,0 +1,125 @@
+import asyncio
+import json
+import indy_sdk, indy
+from indy_sdk import ledger, did, pairwise
+
+# create pool ledger configuration
+pool_handle = indy_sdk.pool.create_pool_ledger_config('pool1', '{"genesis_txn": "path/to/genesis_txn"}')
+
+# create wallet
+wallet_handle = indy_sdk.wallet.create_wallet('wallet1', None, None, None, 'default')
+indy_sdk.wallet.open_wallet('wallet1', None, None)
+
+# define consent schema for vc
+consent_schema_name = "consent"
+consent_schema_version = "1.0"
+consent_schema_attributes = ["user_id", "data_type", "recipient_id", "consent_given"]
+consent_schema_id, consent_schema = indy_sdk.anoncreds.issuer_create_schema("Issuer1", consent_schema_name, consent_schema_version, consent_schema_attributes)
+
+
+#  create consent credential definition
+consent_cred_def_config = {'tag': 'default'}
+consent_cred_def = indy_sdk.anoncreds.issuer_create_and_store_credential_def(wallet_handle, 'Issuer1', consent_schema, 'CL', json.dumps(consent_cred_def_config))
+consent_cred_def_id = consent_cred_def['id']
+
+# function to give consent
+async def give_consent(user_id, data_type, recipient_id):
+    user_did, user_key = await did.create_and_store_my_did(wallet_handle, "{}")
+    recipient_did, recipient_key = await did.create_and_store_my_did(wallet_handle, "{}")
+    pairwise_config = json.dumps({'my_did': user_did, 'their_did': recipient_did})
+    await pairwise.create_pairwise(wallet_handle, pairwise_config)
+    
+    consent_record = {
+        "user_id": user_id,
+        "data_type": data_type,
+        "recipient_id": recipient_id,
+        "consent_given": True
+    }
+    consent_record_json = json.dumps(consent_record)
+    pool_handle = await indy_sdk.pool.open_pool_ledger('pool1', None)
+
+    cred_offer = await indy_sdk.anoncreds.issuer_create_credential_offer(wallet_handle, consent_cred_def_id)
+    send_cred_offer(recipient_id, cred_offer)
+    
+    cred_req, cred_req_metadata = await indy_sdk.anoncreds.prover_create_credential_req(wallet_handle, user_did, cred_offer, consent_cred_def, "{}")
+    cred_values = json.dumps({"attributes": consent_record})
+    cred, _, _ = await indy_sdk.anoncreds.issuer_create_credential(wallet_handle, cred_offer, cred_req, cred_values, None, None)
+    await indy_sdk.anoncreds.prover_store_credential(wallet_handle, None, cred_req_metadata, cred, consent_cred_def, "{}")
+
+      # Write the consent record onto the Indy blockchain
+    credential_request = {
+        'operation': {
+            'type': '101',
+            'config': {
+                'data': json.loads(cred),
+                'source_id': user_id
+            }
+        }
+    }
+    credential_request_json = json.dumps(credential_request)
+    credential_response = await indy_sdk.ledger.sign_and_submit_request(pool_handle, wallet_handle, user_did, credential_request_json)
+
+# function to revoke consent
+async def revoke_consent(user_id, data_type, recipient_id):
+    (user_did, user_key), (recipient_did, recipient_key) = await get_pairwise_dids(wallet_handle, recipient_id)
+    consent_record = {
+        "user_id": user_id,
+        "data_type": data_type,
+        "recipient_id": recipient_id,
+        "consent_given": False
+    }
+    consent_record_json = json.dumps(consent_record)
+
+    cred_offer = await indy_sdk.anoncreds.issuer_create_credential_offer(wallet_handle, consent_cred_def_id)
+
+    await send_cred_offer(recipient_id, cred_offer)
+
+    cred_req, cred_req_metadata = await indy_sdk.anoncreds.prover_create_credential_req(wallet_handle, user_did, cred_offer, consent_cred_def, "{}")
+    cred_values = json.dumps({"attributes": consent_record})
+    cred, _, _ = await indy_sdk.anoncreds.issuer_create_credential(wallet_handle, cred_offer, cred_req, cred_values, None, None)
+    await indy_sdk.anoncreds.prover_store_credential(wallet_handle, None, cred_req_metadata, cred, consent_cred_def, "{}")
+    
+    # Revoke the pairwise connection
+    pairwise_config = json.dumps({'my_did': user_did, 'their_did': recipient_did})
+    await pairwise.delete_pairwise(wallet_handle, pairwise_config)
+
+# function to check if consent has been given to third parties for data exchange 
+async def check_consent(user_id, data_type, recipient_id):
+    (user_did, user_key), (recipient_did, recipient_key) = await get_pairwise_dids(wallet_handle, recipient_id)
+    cred_search_handle = await indy_sdk.anoncreds.prover_search_credentials_for_proof_req(wallet_handle, json.dumps({
+        "nonce": "123456",
+        "name": consent_schema_name,
+        "version": consent_schema_version,
+        "requested_attributes": {
+            "attr1_referent": {"name": "user_id", "restrictions": [{"issuer_did": "Issuer1"}]},
+            "attr2_referent": {"name": "data_type", "restrictions": [{"issuer_did": "Issuer1"}]},
+            "attr3_referent": {"name": "recipient_id", "restrictions": [{"issuer_did": "Issuer1"}]},
+            "attr4_referent": {"name": "consent_given", "restrictions": [{"issuer_did": "Issuer1"}]}
+        }
+    }))
+
+    # Search for the credential that matches the given user_id, data_type, and recipient_id
+    cred_id = None
+    while True:
+        (cred_for_attr, cred_id, _) = await indy_sdk.anoncreds.prover_fetch_credentials_for_proof_req(cred_search_handle, "attr1_referent", 1)
+        if not cred_for_attr:
+            break
+        for value in json.loads(cred_for_attr[0]['cred_info']['attrs']['attr2_referent']):
+            if value == data_type:
+                for value in json.loads(cred_for_attr[0]['cred_info']['attrs']['attr3_referent']):
+                    if value == recipient_id:
+                        cred_id = cred_for_attr[0]['cred_info']['referent']
+                        break
+        if cred_id:
+            break
+
+    # Close the search handle
+    await indy_sdk.anoncreds.prover_close_credentials_search_for_proof_req(cred_search_handle)
+
+    if not cred_id:
+        return False
+
+    # Get the credential and check if consent was given
+    cred = await indy_sdk.anoncreds.prover_get_credential(wallet_handle, cred_id)
+    cred_values = json.loads(cred['values'])
+    return cred_values['attributes']['attr4_referent'] == "True"
---
+++ main.py
@@ -0,0 +1,120 @@
+import asyncio 
+import json
+
+from indy import pool, wallet, did, ledger
+from indy.error import ErrorCode, IndyError
+
+
+
+
+async def create_wallet(identity):
+    print("\"{}\" -> Create wallet".format(identity['name']))
+    try:
+        await wallet.create_wallet(identity['wallet_config'],
+                                   identity['wallet_credentials'])
+    except IndyError as ex:
+        if ex.error_code == ErrorCode.PoolLedgerConfigAlreadyExistsError:
+            pass
+        identity['wallet'] = await wallet.open_wallet(identity['wallet_config'],
+                                                      identity['wallet_credentials'])
+
+
+async def getting_verinym(from_, to):
+    await create_wallet(to)
+
+    (to['did'], to['key']) = await did.create_and_store_my_did(to['wallet'], "{}")
+
+    from_['info'] = {
+        'did': to['did'],
+        'verkey': to['key'],
+        'role': to['role'] or None
+    }
+
+    await send_nym(from_['pool'], from_['wallet'], from_['did'], from_['info']['did'],
+                    from_['info']['verkey'], from_['info']['role'])
+
+async def send_nym(pool_handle, wallet_handle, _did, new_did, new_key, role):
+    nym_request = await ledger.build_nym_request(_did, new_did, new_key, None, role)
+    print(nym_request)
+    await ledger.sign_and_submit_request(pool_handle, wallet_handle, _did, nym_request)
+
+
+async def run():
+    print("Indy demo program")
+    print("STEP 1")
+    pool_ = {
+        "name": "pool1"
+    }
+
+    print("Open Pool Ledger: {}".format(pool_['name']))
+    pool_['genesis_txn_path'] = "pool1.txn"
+    pool_['config'] = json.dumps({"genesis_txn": str(pool_['genesis_txn_path'])})
+
+    print(pool_)
+
+    # connect to the pool
+    await pool.set_protocol_version(2)
+
+    try:
+        await pool.create_pool_ledger_config(pool_['name'], pool_['config'])
+    except IndyError as ex:
+        if ex.error_code == ErrorCode.PoolLedgerConfigAlreadyExistsError:
+            pass
+        pool_['handle'] = await pool.open_pool_ledger(pool_['name'], None)
+
+        print(pool_['handle'])
+
+
+        print("STEP 2 - Configuring steward")
+        steward = {
+            'name': "Sovrin Steward",
+            'wallet_config': json.dumps({'id': 'sovrin_steward_wallet'}),
+            'wallet_credentials': json.dumps({'key': 'steward_wallet_key'}),
+            'pool': pool_['handle'],
+            'seed': '000000000000000000000000Steward1'
+        }
+        print(steward)
+
+        await create_wallet(steward)
+
+        print(steward["wallet"])
+
+        steward["did_info"] = json.dumps({'seed': steward['seed']})
+        print(steward["did_info"])
+
+        #did:demoindynetwork:Th7MpTaRZVRYnPiabds81Y
+        steward['did'], steward['key'] = await did.create_and_store_my_did(steward['wallet'], steward['did_info '])
+
+        print("STEP 3 - register DID for university and company")
+
+        print("\n\n\n================================")
+        print("==   theUniversity getting Verinym   ==")
+        print('--------------------------------')
+
+        theUniversity = {
+            'name': 'theUniversity',
+            'wallet_config': json.dumps({'id': 'theUniversity_wallet'}),
+            'wallet_credentials': json.dumps({'key': 'theUniversity_wallet_key'}),
+            'pool': pool_['handle'],
+            'role': 'TRUST_ANCHOR'
+        }
+
+        await getting_verinym(steward, theUniversity)
+
+        print("\n\n\n================================")
+        print("==   theCompany getting Verinym   ==")
+        print('--------------------------------')
+
+        theCompany = {
+            'name': 'theUniversity',
+            'wallet_config': json.dumps({'id': 'theCompany_wallet'}),
+            'wallet_credentials': json.dumps({'key': 'theCompany_wallet_key'}),
+            'pool': pool_['handle'],
+            'role': 'TRUST_ANCHOR'
+        }
+
+        await getting_verinym(steward, theCompany)  
+
+
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(run())
